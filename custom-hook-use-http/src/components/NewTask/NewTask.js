9762efa029054064f48f6bfc0521f195813448aa
Yixin Jiang <jiangyixin0910@gmail.com>
2021-06-19 22:54:41 +0200

    Add custom app with an example of useHttp

---
+++ custom-hook-use-http/src/components/NewTask/NewTask.js
@@ -0,0 +1,37 @@
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: createTaskRequest } = useHttp();
+
+  const enterTaskHandler = async (taskText) => {
+    const transformeTask = (taskText, data) => {
+      const generatedId = data.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+
+      props.onAddTask(createdTask);
+    };
+
+    createTaskRequest(
+      {
+        url: "https://react-http-7b554-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      transformeTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
